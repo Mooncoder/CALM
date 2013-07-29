30fbae89ec04e7c758c321848838bc6dee0e5e92
Iain E. Davis <iain@ruhlendavis.org>
2013-07-29 09:58:02 -0500

    Adjusted primary class to be a JavaPlugin class.

--- src/org/ruhlendavis/mc/calm/Calm.java
+++ src/org/ruhlendavis/mc/calm/Calm.java
@@ -1,10 +1,12 @@
 package org.ruhlendavis.mc.calm;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
 /**
  *
  * @author Iain E. Davis <iain@ruhlendavis.org>
  */
-public class Calm
+public final class Calm extends JavaPlugin
 {
 
 }
