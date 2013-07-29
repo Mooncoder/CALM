4d10e1e0d7486bc483eb829a8a573336e839dd33
Iain E. Davis <iain@ruhlendavis.org>
2013-07-29 10:00:22 -0500

    Descriptive updates.

--- src/org/ruhlendavis/mc/calm/Calm.java
+++ src/org/ruhlendavis/mc/calm/Calm.java
@@ -3,7 +3,8 @@ package org.ruhlendavis.mc.calm;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
- *
+ * Primary class.
+ * 
  * @author Iain E. Davis <iain@ruhlendavis.org>
  */
 public final class Calm extends JavaPlugin
