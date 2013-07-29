a9a7b0c128a8b1d9820428924bd9d818945f932b
Iain E. Davis <iain@ruhlendavis.org>
2013-07-29 10:19:53 -0500

    Added class skeleton.

---
+++ src/org/ruhlendavis/mc/calm/LoggingFilter.java
@@ -0,0 +1,17 @@
+package org.ruhlendavis.mc.calm;
+
+import java.util.logging.Filter;
+import java.util.logging.LogRecord;
+
+/**
+ *
+ * @author Iain E. Davis <iain@ruhlendavis.org>
+ */
+public class LoggingFilter implements Filter
+{
+	@Override
+	public boolean isLoggable(LogRecord record)
+	{
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+}
