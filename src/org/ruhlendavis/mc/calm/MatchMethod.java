4fa38f5e5b95cdf26b463fed2dbb8c1a6ea39ccf
Iain E. Davis <iain@ruhlendavis.org>
2013-07-29 10:15:32 -0500

    Added match method class.

---
+++ src/org/ruhlendavis/mc/calm/MatchMethod.java
@@ -0,0 +1,14 @@
+package org.ruhlendavis.mc.calm;
+
+/**
+ * Enumeration for the various methods of matching.
+ * 
+ * @author Iain E. Davis <iain@ruhlendavis.org>
+ */
+public enum MatchMethod
+{
+	EXACT,
+	REGEX,
+	SIMPLE,
+	WILDCARD,
+}
