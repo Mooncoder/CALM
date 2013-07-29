89822137b3d8854200552053407d2f0a1ee0b5a3
Iain E. Davis <iain@ruhlendavis.org>
2013-07-29 11:10:43 -0500

    Added skeleton of command executor.

---
+++ src/org/ruhlendavis/mc/calm/CalmCommandExecutor.java
@@ -0,0 +1,57 @@
+package org.ruhlendavis.mc.calm;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+/**
+ *
+ * @author Feaelin (Iain E. Davis) <iain@ruhlendavis.org>
+ */
+public class CalmCommandExecutor implements CommandExecutor
+{
+	/**
+	 * Bukkit will pass control to onCommand for any of our commands.
+	 * 
+	 * @param sender    Initiator of the command
+	 * @param command   Command object of the command
+	 * @param label     Actual alias used by the initiator
+	 * @param arguments Any arguments provided by the initiator
+	 * @return 
+	 */
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] arguments)
+	{
+		int argumentStart = 0;
+		String commandName = command.getName().toLowerCase();
+		
+		// If user types 'calm somecommand arg1 arg2' set commandName, and note
+		// where the arguments start.
+		if (commandName.equals("calm"))
+		{
+			commandName = arguments[argumentStart];
+			argumentStart++;
+		}
+		
+		// If user types 'calmsomecommand arg1 arg2' then calmsomecommand --> somecommand.
+		if (commandName.startsWith("calm"))
+		{
+			commandName = commandName.substring(4, commandName.length());
+		}
+		
+		// Amusing side effect of above. calm calmreload will work. :)
+		
+		if (commandName.equals("reload"))
+		{
+			onReloadConfig();
+		}
+		
+		return true;
+	}
+	/**
+	 *  Response to the calm reload command.
+	 */
+	private void onReloadConfig()
+	{
+	}
+}
